2c07cb7446e5850a9e55c22b002d99fff44c567c
Mishkat Hossain <hossainmishkat0@gmail.com>
2025-09-27 22:54:20 +0600

    vkkkv

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <h1>This is about</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eos possimus ex reiciendis dolores excepturi placeat cupiditate aperiam quae eaque sint quis esse est provident iure ipsum tempora, iste quibusdam.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
     return view('welcome');
 });
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::view('/about', 'about');
+
+
+Route::get('/product/{category}', function(string $category) {
+    return "Current Category: $category";
+})->whereAlpha('category');
+
+
+
+Route::get('user/{id}', function (int $id){
+    return "Working";
+})->whereNumber('id', '{10,20}');
