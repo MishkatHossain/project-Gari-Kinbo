54f590c12acd5d389572dab287687cbfe2a44bf7
Mishkat Hossain <hossainmishkat0@gmail.com>
2025-09-30 08:18:45 +0600

    ..

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,25 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\ShowCarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
     return view('welcome');
 });
 
-Route::get('/welcome', function () {
-    return view('welcome');
+// Route::controller(CarController::class)->group(function(){
+//     Route::get('/car', 'index');
+//     Route::get('/my-cars', 'myCars');
+// });
+
+
+Route::get('/car', [ShowCarController::class]);
+
+// Route::get('/car', [CarController::class, 'index']);
+
+Route::get('/car', [ShowCarController::class]);
+
+Route::fallback(function () {
+    return "Nah! Just an Error";
 });
-
-Route::view('/about', 'about');
-
-
-Route::get('/product/{category}', function(string $category) {
-    return "Current Category: $category";
-})->whereAlpha('category');
-
-
-
-Route::get('user/{id}', function (int $id){
-    return "Working";
-})->whereNumber('id', '{10,20}');
