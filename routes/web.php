6081675d84dc1966511df2f309d7b899a5b852c0
Mishkat Hossain <hossainmishkat0@gmail.com>
2025-10-10 10:58:19 +0600

    long long tribe

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
-
+use App\Http\Controllers\SignupController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/signup', [SignupController::class, 'create']);
+
+
+
+
+
+
+
+
+
